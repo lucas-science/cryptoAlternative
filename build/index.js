1bed0388fb2c1639db6b5dbfcc185e7f18cbeed9
lucas-science <lucaslhomme01@gmail.com>
2022-03-02 19:39:56 +0100

    add functions comments

--- build/index.js
+++ build/index.js
@@ -1,20 +1,109 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const callEndpoints_1 = require("./lib/callEndpoints");
-/*
-Ticker({limit:5,start:2,convert:'EUR',sort:'id'}).then(res => {
-    console.log("lol",res)
-})
+exports.GetFearIndexPhotoDate = exports.GetFearIndexPhoto = exports.GetFearIndex = exports.GetGlobal = exports.GetTicker = exports.GetTickerSpecificCurrency = void 0;
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
+const endpoints_1 = __importDefault(require("./lib/endpoints"));
+const controllers_1 = require("./lib/controllers");
+/**
+ *
+ * @param {Object} parameters with limit?, start?, convert?, structure?, sort?. If no parameters gived, function return ALL Ticker
+ * @returns {Promise} data get with the API request
+ * @example ```js
+ * const crypto = require('@luxlhm/crypto_alternative')
+ *
+ * crypto.GetTicker({limit: 2,convert: 'EUR'}).then(data => console.log(data))
+ * ```
 */
-/*
-TokenNameIsOnListings("bitcoin").then(res=>{
-    console.log(res)
-})*/
-/*
-Global().then(res=> console.log(res))
-*/
-(0, callEndpoints_1.FearIndexPhoto)({
-    name: 'talalala'
-})
-    .then(r => console.log(r))
-    .catch(err => console.log(err));
+const GetTicker = async (parametres) => {
+    if (parametres) {
+        const url = `${endpoints_1.default.baseURL}/v2/ticker/?${parametres.limit != undefined ? `limit=${parametres.limit}` : ``}${parametres.start != undefined ? `&start=${parametres.start}` : ``}${parametres.convert != undefined ? `&convert=${parametres.convert}` : ``}${parametres.structure != undefined ? `&structure=${parametres.structure}` : ``}${parametres.sort != undefined ? `&sort=${parametres.sort}` : ``}`;
+        console.log(url);
+        return await (0, controllers_1.CallAPI)(url);
+    }
+    else {
+        const url = `${endpoints_1.default.baseURL}/v2/ticker/`;
+        return await (0, controllers_1.CallAPI)(url);
+    }
+};
+exports.GetTicker = GetTicker;
+/**
+ *
+ * @param {Number or String} id
+ * @returns {Promise}
+ * @example ```js
+ *
+ *
+ * ```
+ */
+const GetTickerSpecificCurrency = async (id) => {
+    const url = `${endpoints_1.default.baseURL}/v2/ticker/${id}/`;
+    return await (0, controllers_1.CallAPI)(url);
+};
+exports.GetTickerSpecificCurrency = GetTickerSpecificCurrency;
+GetTickerSpecificCurrency('bitcoin').then(e => console.log(e));
+const GetGlobal = async (parametres) => {
+    if (parametres) {
+        const url = `${endpoints_1.default.baseURL}/v2/global/?${parametres.convert != undefined ? `convert=${parametres.convert}` : ``}`;
+        console.log(url);
+        return await (0, controllers_1.CallAPI)(url);
+    }
+    else {
+        const url = `${endpoints_1.default.baseURL}/v2/global/`;
+        return await (0, controllers_1.CallAPI)(url);
+    }
+};
+exports.GetGlobal = GetGlobal;
+const GetFearIndex = async (parametres) => {
+    if (parametres) {
+        const url = `${endpoints_1.default.baseURL}/fng/?${parametres.limit != undefined ? `limit=${parametres.limit}` : ``}${parametres.format != undefined ? `&format=${parametres.format}` : ``}${parametres.date_format != undefined ? `&date_format=${parametres.date_format}` : ``}`;
+        console.log(url);
+        return await (0, controllers_1.CallAPI)(url);
+    }
+    else {
+        const url = `${endpoints_1.default.baseURL}/fng/`;
+        return await (0, controllers_1.CallAPI)(url);
+    }
+};
+exports.GetFearIndex = GetFearIndex;
+const GetFearIndexPhoto = async (parametres) => {
+    const url = `${endpoints_1.default.imageURL}/crypto/fear-and-greed-index.png`;
+    if (parametres.RelativePath) {
+        if (!fs_1.default.existsSync(__dirname + `${parametres.RelativePath}`)) { // if folder doesnt exist
+            await fs_1.default.promises.mkdir(__dirname + parametres.RelativePath, { recursive: true });
+            const PATH = path_1.default.resolve(__dirname, `${parametres.RelativePath.replace('/', '')}`, `${parametres.name}.png`);
+            return await (0, controllers_1.DownloadIMG)(PATH, url);
+        }
+        else { // if folder exist
+            const PATH = path_1.default.resolve(__dirname, `${parametres.RelativePath.replace('/', '')}`, `${parametres.name}.png`);
+            return await (0, controllers_1.DownloadIMG)(PATH, url);
+        }
+    }
+    else {
+        const PATH = path_1.default.resolve(__dirname, `${parametres.name}.png`);
+        return await (0, controllers_1.DownloadIMG)(PATH, url);
+    }
+};
+exports.GetFearIndexPhoto = GetFearIndexPhoto;
+const GetFearIndexPhotoDate = async (parametres) => {
+    const url = `${endpoints_1.default.imageURL}/images/fng/crypto-fear-and-greed-index-${parametres.date.year}-${parametres.date.month}-${parametres.date.day}.png`;
+    if (parametres.RelativePath) {
+        if (!fs_1.default.existsSync(__dirname + `${parametres.RelativePath}`)) { // if folder doesnt exist
+            await fs_1.default.promises.mkdir(__dirname + parametres.RelativePath, { recursive: true });
+            const PATH = path_1.default.resolve(__dirname, `${parametres.RelativePath.replace('/', '')}`, `${parametres.name}.png`);
+            return await (0, controllers_1.DownloadIMG)(PATH, url);
+        }
+        else { // if folder exist
+            const PATH = path_1.default.resolve(__dirname, `${parametres.RelativePath.replace('/', '')}`, `${parametres.name}.png`);
+            return await (0, controllers_1.DownloadIMG)(PATH, url);
+        }
+    }
+    else {
+        const PATH = path_1.default.resolve(__dirname, `${parametres.name}.png`);
+        return await (0, controllers_1.DownloadIMG)(PATH, url);
+    }
+};
+exports.GetFearIndexPhotoDate = GetFearIndexPhotoDate;
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,17 @@ import endpoints from "./lib/endpoints"
 import { TickerConfig,GlobalConfig, FearIndexConfig, FearIndexPhotoConfig, FearIndexPhotoDateConfig } from "./lib/interface"
 import {CallAPI, DownloadIMG} from "./lib/controllers"
 
+/**
+ * 
+ * @param {Object} parameters with limit?, start?, convert?, structure?, sort?. If no parameters gived, function return ALL Tickers. 
+ * @returns {Promise} return list of Crypto currency ticker
+ * @example ```js
+ * const crypto = require('@luxlhm/crypto_alternative')
+ * crypto.GetTicker().then(data => console.log(data))
+ * // or
+ * crypto.GetTicker({limit: 2,convert: 'EUR'}).then(data => console.log(data))
+ * ``` 
+*/
 const GetTicker = async (parametres?:TickerConfig):Promise<object> => {
     if(parametres){
         const url = `${endpoints.baseURL}/v2/ticker/?${ parametres.limit != undefined ? `limit=${parametres.limit}`:``}${ parametres.start != undefined ? `&start=${parametres.start}`:`` }${ parametres.convert != undefined ? `&convert=${parametres.convert}`:`` }${ parametres.structure != undefined ? `&structure=${parametres.structure}`:`` }${ parametres.sort != undefined ? `&sort=${parametres.sort}`:`` }`
@@ -16,11 +27,37 @@ const GetTicker = async (parametres?:TickerConfig):Promise<object> => {
     }
 }
 
+/**
+ * 
+ * @param {Number or String} id give crypto id or name, bitcoin id is 1 or 'bitcoin'
+ * @returns {Promise} return data of a specifiy crypto currency
+ * @example ```js
+ * const crypto = require('@luxlhm/crypto_alternative')
+ * 
+ * crypto.GetTickerSpecificCurrency('bitcoin').then(data => console.log(data))
+ * // or 
+ * crypto.GetTickerSpecificCurrency('bitcoin').then(data => console.log(data))
+ * 
+ * ```
+ */
 const GetTickerSpecificCurrency = async (id:Number|String):Promise<object> => {
-    const url = `${endpoints.baseURL}/v2/ticker/${id}`
+    const url = `${endpoints.baseURL}/v2/ticker/${id}/`
     return await CallAPI(url)
 }
 
+/**
+ * 
+ * @param {Object} parametres give convet? param to convert values
+ * @returns {Promise} return overview of all crytpos currencys
+ * @example ```js
+ * const crypto = require('@luxlhm/crypto_alternative')
+ * 
+ * crypto.GetBlobal().then(data => console.log(data))
+ * // or 
+ * crypto.GetBlobal({convert:'EUR'}).then(data => console.log(data))
+ * 
+ * ```
+ */
 const GetGlobal = async (parametres?:GlobalConfig):Promise<object> => {
     if(parametres){
         const url = `${endpoints.baseURL}/v2/global/?${ parametres.convert != undefined ? `convert=${parametres.convert}`:``}`
@@ -32,6 +69,18 @@ const GetGlobal = async (parametres?:GlobalConfig):Promise<object> => {
     }
 }
 
+/**
+ * 
+ * @param {Object} parametres give limit?, format?, date_format?, to get fear index. If you gived no parameters you will get the last fear index
+ * @returns {Promise} return fear index
+ * @example ```js
+ * const crypto = require('@luxlhm/crypto_alternative')
+ * 
+ * crypto.GetFearIndex().then(data => console.log(data))
+ * // or
+ * crypto.GetFearIndex({limit:2, date_format:'world', format:'json'}).then(data => console.log(data)) 
+ * ```
+ */
 const GetFearIndex = async (parametres?:FearIndexConfig):Promise<object> => {
     if(parametres){
         const url = `${endpoints.baseURL}/fng/?${ parametres.limit != undefined ? `limit=${parametres.limit}`:``}${parametres.format != undefined ? `&format=${parametres.format}`:``}${parametres.date_format != undefined ? `&date_format=${parametres.date_format}`:``}`
@@ -43,6 +92,18 @@ const GetFearIndex = async (parametres?:FearIndexConfig):Promise<object> => {
     }
 }
 
+/**
+ * 
+ * @param {Object} parametres give : name(name to give of the downloaded image), RelativePath?(location where downloaded file will be).
+ * @returns {Promise} return The last fear index photo
+ * @example ```js
+ * const crypto = require('@luxlhm/crypto_alternative')
+ * 
+ * crypto.GetFearIndexPhoto({name:'monday_fearindex'}).then(data => console.log(data))
+ * // or
+ * crypto.GetFearIndexPhoto({name:'monday_fearindex', RelativePath:'/PhotoDist'}).then(data => console.log(data)) 
+ * ```
+ */
 const GetFearIndexPhoto = async (parametres:FearIndexPhotoConfig):Promise<any> => {
     const url = `${endpoints.imageURL}/crypto/fear-and-greed-index.png`
     if(parametres.RelativePath){
@@ -60,7 +121,20 @@ const GetFearIndexPhoto = async (parametres:FearIndexPhotoConfig):Promise<any> =
     }
 }
 
-const GetFearIndexPhotoDate = async (parametres:FearIndexPhotoDateConfig) => {
+
+/**
+ * 
+ * @param {Object} parametres give : name(name to give of the downloaded image), RelativePath?(location where downloaded file will be), date{day:number,month:number,year:number}.
+ * @returns {Promise} return fear index photo of the day gived, if the photo exist for the date gived.
+ * @example ```js
+ * const crypto = require('@luxlhm/crypto_alternative')
+ * 
+ * crypto.GetFearIndexPhotoDate({name:'monday_fearindex').then(data => console.log(data))
+ * // or
+ * crypto.GetFearIndexPhotoDate({name:'monday_fearindex', RelativePath:'/PhotoDist', date:{day:20,month:3, year:2021}}).then(data => console.log(data)) 
+ * ```
+ */
+const GetFearIndexPhotoDate = async (parametres:FearIndexPhotoDateConfig):Promise<any> => {
     const url = `${endpoints.imageURL}/images/fng/crypto-fear-and-greed-index-${parametres.date.year}-${parametres.date.month}-${parametres.date.day}.png`
     if(parametres.RelativePath){
         if(!Fs.existsSync(__dirname+`${parametres.RelativePath}`)){ // if folder doesnt exist
